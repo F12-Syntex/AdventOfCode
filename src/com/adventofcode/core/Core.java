68417ad99c3338c102abca828a7329148ef8ef38
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-03-13 10:36:10 +0000

    Added core
    added some basic things to help get the other days started.

---
+++ src/com/adventofcode/core/Core.java
@@ -0,0 +1,8 @@
+package com.adventofcode.core;
+
+public abstract class Core {
+ 
+    public abstract void part1();
+    public abstract void part2();
+
+}
---
+++ src/com/adventofcode/year22/Day1.java
@@ -0,0 +1,16 @@
+package com.adventofcode.year22;
+
+public class Day1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public void part1() {
+        System.out.println("Hello World!");
+    }
+    
+    public void part2() {
+        System.out.println("Hello World!");
+    }
+}
